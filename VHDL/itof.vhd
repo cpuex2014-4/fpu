78c43d7068afe02c36d9f09f8e0c819df5938cfe
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-10-23 17:09:20 +0900

    itof is pipelined

--- VHDL/itof.vhd
+++ VHDL/itof.vhd
@@ -7,6 +7,7 @@ use IEEE.std_logic_unsigned.all;
 
 entity ITOF is
   Port (input  : in  std_logic_vector(31 downto 0);
+        clk : in std_logic;
         output : out std_logic_vector(31 downto 0));
 end entity ITOF;
 
@@ -16,14 +17,15 @@ architecture RTL of ITOF is
     port (
       input1 : in int32;
       input2 : in int32;
+      clk: std_logic;
       output : out int32);
   end component;
   signal t: int32;
-  signal low0, high0, low, high, a, b, ans: int32;
+  signal low0, high0, low, high, a, b, a1, b1, ans: int32;
 begin
-  lowsub:  FADD port map (input1=> low,  input2=> x"cb000000", output=>a);
-  highsub: FADD port map (input1=> high, input2=> x"d6800000", output=>b);
-  lhadd:   FADD port map (input1=> a,    input2=> b, output=>ans);
+  lowsub:  FADD port map (input1=> low,  input2=> x"cb000000", clk => clk, output=>a1);
+  highsub: FADD port map (input1=> high, input2=> x"d6800000", clk => clk, output=>b1);
+  lhadd:   FADD port map (input1=> a,    input2=> b, clk => clk, output=>ans);
 
   t <= input when input(31) = '0' else
        (not input) + 1;
@@ -34,6 +36,29 @@ begin
   low  <= low0 + x"4b000000";
   high <= high0 + x"56800000";
 
-  output <= input(31)&ans(30 downto 0);
+
+
+  itof_proc: process(input, clk)
+    variable sign1 : std_logic;
+    variable sign2 : std_logic;
+    variable sign3 : std_logic;
+    variable sign4 : std_logic;
+    variable sign5 : std_logic;
+  begin
+    if rising_edge(clk) then
+
+      output <= sign5&ans(30 downto 0);
+
+      sign5 := sign4;
+      sign4 := sign3;
+      sign3 := sign2;
+      sign2 := sign1;
+      sign1 := input(31);
+
+      a <= a1;
+      b <= b1;
+
+    end if;
+  end process;
 
 end architecture RTL;
--- VHDL/itof_test.vhd
+++ VHDL/itof_test.vhd
@@ -15,33 +15,39 @@ architecture Behavior of top is
   component ITOF is
   Port (
     input : in  int32;
+    clk : in std_logic;
     output : out int32);
   end component;
 
   signal a:int32 := (others=>'0');
   signal ans:int32 := (others=>'0');
--- signal clk:std_logic := '0';
+  signal clk:std_logic := '0';
   file  read_file  : text open read_mode  is "i2f.in";
   file  write_file : text open write_mode is "i2f.out";
 begin
-  hoge: ITOF port map (input=>a,output=>ans);
+  hoge: ITOF port map (input=>a, clk=>clk, output=>ans);
 
-  readProc:process
+  readProc:process(clk)
     variable lin : line;
     variable ra : int32;
     variable wans : int32;
     variable lout : line;
   begin
-    while not(endfile(read_file)) loop
+    if rising_edge(clk) then
       readline(read_file, lin);
       hread(lin, ra);
       a <= ra;
-      wait for 2 ns;
-      wans:=ans;
-      hwrite(lout, wans);
+    end if;
+    if falling_edge(clk) then
+      hwrite(lout, ans);
       writeline(write_file, lout);
-    end loop;
-    wait;
+    end if;
   end process;
-
+  clkgen: process
+  begin
+    clk<='0';
+    wait for 5 ns;
+    clk<='1';
+    wait for 5 ns;
+  end process;
 end Behavior;
