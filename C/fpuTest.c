18cf1f5ae48837f524a394378914355feb160e41
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-11-04 22:24:16 +0900

    implement All-Number check

--- C/fpuTest.c
+++ C/fpuTest.c
@@ -5,7 +5,7 @@
 
 int main (int argc, char* argv[]) {
 
-  if (argc != 2) {
+  if (argc < 2) {
     puts("16進数でテストケースを与えると、結果を標準出力に、間違えたケースを標準エラー出力に出力します");
     puts("使用例: ./fpuTest fadd");
     return 0;
@@ -110,6 +110,47 @@ int main (int argc, char* argv[]) {
       print_bits(a);
       printf("float:%.10f int:%u\n", a.f, a.u);
     }
+
+  } else if (strcmp(argv[1], "-all") == 0 && argc == 3) {
+    uni a;
+    uint32_t i = 0;
+    int flg = 0;
+    if (strcmp(argv[2], "ftoi") == 0) {
+      flg = 1;
+    } else if (strcmp(argv[2], "itof") == 0) {
+      flg = 2;
+    } else if (strcmp(argv[2], "finv") == 0) {
+      flg = 3;
+    } else if (strcmp(argv[2], "fsqrt") == 0) {
+      flg = 4;
+    }
+    if (flg == 0) {
+      fprintf(stderr, "Error");
+      return 0;
+    }
+    do {
+      if (i % (1 << 24) == 0) {
+        printf("%lf%%\n", (double)i*100/pow(2, 32));
+      }
+      a.u = i;
+      switch (flg) {
+      case 1:
+        ftoiCheck(a);
+        break;
+      case 2:
+        itofCheck(a);
+        break;
+      case 3:
+        finvCheck(a);
+        break;
+      case 4:
+        fsqrtCheck(a);
+        break;
+      default:
+        break;
+      }
+      i++;
+    } while(i != 0);
 
   } else {
 
